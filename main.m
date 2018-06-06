e5d62bbb4dd6f7fb59c61cb3f845fec65cc980f4
Michael Johnston <MichaelTJohnston@Gmail.com>
2018-06-05 21:10:01 -0700

    Solved for max data rate to meet link budget. More constants and data added from spec sheets.

--- main.m
+++ main.m
@@ -1,11 +1,15 @@
-% Code by Michael Johnston, June 4, 2018
+% Code by Michael Johnston, Valeria Salazar, Bryan Num, Chris Barta, June 4, 2018
 % AERO 446 Project
+
+% -- Assumptions --
+% Pointing errors are negligible (XACT-50 has ?0.003 deg accuracy)
 
 % housekeeping
 clear; % Clears workspace
 clc; %clears command window
 close all;
 
+% -- Constants & givens for equipment --
 c = 299.8E6; % Speed of light [m/s]
 hOrbit = 200; % Height of orbit [km]
 muMoon = 4904.8695; % Gravitational constant of Moon [km^3/s^2]
@@ -15,14 +19,48 @@ EMdist = 406700E3; % Max distance from Earth to Moon [m]
 pSens = 40; % Power consumed by sensor during science gathering [w]
 pSens_sby = 2; % Power consumed by sensor in stand-by [w]
 rSens = 100E3; % Data rate of sensor during operations [bps]
+
+fRange = linspace(8.400, 8.450)'*1E9; % X-Band frequency range [Hz]
 commTime = 2*3600; % Total required comm time per day [s]
+txPower = 35; % Power consumed by radio during transmit/receive [W]
+rxPower = 12.5; % Power consumde by radio during receive only [W]
+
 diaG = 20; % Diameter of ground antenna [m]
 diaScFixed = 20E-2; % Diameter of FIXED spacecraft antenna (low-cost) [m]
 diaScDep = 50E-2; % Diameter of DEPLOYABLE spacecraft antenna (high-cost) [m]
 
-% -- dB Equations
+% -- dB Equations --
 G = @(D,f) 20*log10(f*1E-9) + 20*log10(D) + 17.8; % Parabolic antenna gain [dB]
 Ls = @(dist,f) 20*log10(dist) + 20*log10(f) - 147.55; % Path loss [dB]
-beamWid = @(D,f) 65.3*c/f/D; % Parabolic antenna beam width equation [degrees]
-Lpoint = @(err,beamWid) 12*(err/beamWid)^2; % Pointing error equation [dB]
+beamWid = @(D,f) 65.3*c./f/D; % Parabolic antenna beam width equation [degrees]
+Lpoint = @(err,beamWid) 12*(err./beamWid).^2; % Pointing error equation [dB]
 
+Ts = 10*log10(150); % System temperature noise [K]
+Ll = 5; % Line losses [dB]
+EbNoMin = 3 + 5; % Link budget plus margin for Reed-Solomon encoding[dB]
+Latm = 0; % Atmospheric losses ASSUMING ZERO [dB]
+Lpt = 0; % Pointing losses [dB]
+
+% -- Signal loss vs Frequency (tl;dr, frequency doesn't matter)
+figure
+plot(fRange*1E-9, G(diaG,fRange) + G(diaScDep,fRange) - Ls(EMdist,fRange),...
+	fRange*1E-9, G(diaG,fRange) + G(diaScFixed,fRange) - Ls(EMdist,fRange), 'lineWidth', 2)
+grid on
+title('Signal Loss vs Frequency (Antenna Gain - Path Loss)')
+xlabel('Frequency [GHz]'), ylabel('Signal Loss [dB]')
+legend('Deployable Antenna', 'Fixed Antenna')
+
+% Solving for data rate that meets link budget
+fTx = fRange(1); % Transmit frequency
+syms dRate
+% Max rate [bps]
+dRate = double(solve(EbNoMin == 10*log10(txPower) - Ls(EMdist,fTx) +...
+	G(diaG,fTx) + G(diaScFixed,fTx) + 228.6 - Ts - 10*log(dRate) - Ll,dRate));
+
+
+
+function res = linkEq(factors,solveFor)
+	syms EbNo P Ls Lpt_r Lpt_t Latm Gr Gt Ts R Ll
+	
+	eq = EbNo == P - Ls - Lpt_r - Lpt_t - Latm + Gr + Gt - 10*log10(Ts) - 10*log10(R) - Ll;
+end
