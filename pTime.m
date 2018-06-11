f60a8135e40755ba03d63a2fe36fa67f21136b55
Michael Johnston <MichaelTJohnston@Gmail.com>
2018-06-10 22:10:47 -0700

    Updated ptime for more dankness

--- pTime.m
+++ pTime.m
@@ -5,17 +5,22 @@
 %		  T = Period
 %		  tEcl = Eclipse time range
 %		  time = Time span
+%		  orbits = orbit vector you want included
+%		  true = only include eclipse
 
-function power = pTime(P,T,tEcl,time, orbits)
-	power = [linspace(0,time)', zeros(100,1)];
+function power = pTime(P,T,tEcl,time, orbits, varargin)
+	res = 10000;
+	power = [linspace(0,time,res)', zeros(res,1)];
 	
 	orb = 1;
-	for i = 1:100
+	for i = 1:res
 		if power(i,1) > orb*T
 			tEcl = tEcl + T;
-			orb = orb + 1
+			orb = orb + 1;
 		end
-		if power(i,1) > tEcl(1) && power(i,1) < tEcl(2) && ismember(orb,orbits)
+		if power(i,1) > tEcl(1) && power(i,1) < tEcl(2) && ismember(orb,orbits) && nargin < 6
+			power(i,2) = P;
+		elseif (power(i,1) < tEcl(1) || power(i,1) > tEcl(2)) && ismember(orb,orbits)
 			power(i,2) = P;
 		else
 			power(i,2) = 0;
