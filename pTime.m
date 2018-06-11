0056553b107ef2e9ae75f52eb45a5b6d0d6b1e25
Michael Johnston <MichaelTJohnston@Gmail.com>
2018-06-11 01:50:40 -0700

    Allowed for time-variant power plots in ptime (just supply a function handle instead of constant P). Updated plots

--- pTime.m
+++ pTime.m
@@ -14,20 +14,26 @@ function power = pTime(P,T,tEcl,time, orbits, varargin)
 	
 	orb = 1;
 	for i = 1:res
-		if power(i,1) > orb*T
+		t = power(i,1);
+		if isa(P, 'function_handle') % Check if P varies with time
+			p = max(P(t),0);
+		else
+			p = P;
+		end
+		if t > orb*T
 			if norm(tEcl) > 0
 				tEcl = tEcl + T;
 			end
 			orb = orb + 1;
 		end
 		if norm(tEcl) == 0 && ismember(orb,orbits) % No eclipse
-			power(i,2) = P;
+			power(i,2) = p;
 		elseif norm(tEcl) == 0 % No eclipse, no power
 			power(i,2) = 0;
-		elseif power(i,1) > tEcl(1) && power(i,1) < tEcl(2) && ismember(orb,orbits) && nargin < 6
-			power(i,2) = P;
-		elseif (power(i,1) < tEcl(1) || power(i,1) > tEcl(2)) && ismember(orb,orbits) && nargin > 5 % Only eclipse
-			power(i,2) = P;
+		elseif t > tEcl(1) && t < tEcl(2) && ismember(orb,orbits) && nargin < 6
+			power(i,2) = p;
+		elseif (t < tEcl(1) || t > tEcl(2)) && ismember(orb,orbits) && nargin > 5 % Only eclipse
+			power(i,2) = p;
 		else
 			power(i,2) = 0;
 		end
